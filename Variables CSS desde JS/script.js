5d03b834895767e669f0db33afde9508bdae2500
Camilo Delgado <camilod@createme.com>
2023-02-12 12:07:15 -0500

    Adding Finished Variables CSS Test

--- Variables CSS desde JS/script.js
+++ Variables CSS desde JS/script.js
@@ -1,6 +1,17 @@
  const style = document.documentElement.style
 
+ const handelChangeFontFamily = () =>{
+    value = document.getElementsByTagName('select')[0].value
+    style.setProperty('--font-family', value)
+ }
+
  const handleSwitchTheme = () => {
-    style.setProperty('--background', '#444')
-    style.setProperty('--color', '#f3f3f3')
+    if(style.getPropertyValue('--background')==='#f3f3f3'){
+        style.setProperty('--background', '#444');
+        style.setProperty('--color', '#f3f3f3');
+    } 
+    else{
+        style.setProperty('--background', '#f3f3f3');
+        style.setProperty('--color', '#444');
+    }
  }
